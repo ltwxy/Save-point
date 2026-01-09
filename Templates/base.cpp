fdb9758ab26e4a609c457dab5cca80b57f32a856
ltwxy <3285433514@qq.com>
2026-01-09 21:30:16 +0800

    Templates/base.cpp - 标准起手式

---
+++ Templates/base.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int t;
+void solve(){
+	
+}
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	
+	return 0;
+}
+
