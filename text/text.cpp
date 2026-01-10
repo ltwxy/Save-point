99c7cc1a774d1b1ec942d6523a28f78c4b65d507
ltwxy <3285433514@qq.com>
2026-01-10 11:01:47 +0800

    text/text.cpp - 测试文件

---
+++ text/text.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int t;
+void solve(){
+    cout<<"hello world!\n";
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
