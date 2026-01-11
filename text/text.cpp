a74da9f50feca9b26bac47970b8a3a653f592c4d
ltwxy <3285433514@qq.com>
2026-01-11 13:09:01 +0800

    text/text.cpp - 测试

--- text/text.cpp
+++ text/text.cpp
@@ -2,13 +2,34 @@
 #define ll long long
 using namespace std;
 int t;
+struct node{
+    int f;
+    string s;
+};
 void solve(){
-    cout<<"hello world!\n";
+    int n,m;
+    cin>>n>>m;
+    vector<node> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i].f>>a[i].s;
+    }
+    int tap=0;
+    for(int i=0;i<m;i++){
+        int op,x;
+        cin>>op>>x;
+        if(op==a[tap].f){
+            tap=(tap+n-x)%n;
+        }else{
+            tap=(tap+x)%n;
+        }
+    }
+    cout<<a[tap].s<<'\n';
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>t;
+    //cin>>t;
+    t=1;
     while(t--){
         solve();
     }
