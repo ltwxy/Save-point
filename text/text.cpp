24643645c0c33bcc68aae9bb4ee5fb3fc699997d
ltwxy <3285433514@qq.com>
2026-01-12 19:51:55 +0800

    text/text.cpp - 测试

--- text/text.cpp
+++ text/text.cpp
@@ -1,38 +1,19 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <iostream>
+#include <cstdlib> // 包含 rand() 和 srand()
+#include <ctime>   // 包含 time()
+
 using namespace std;
-int t;
-struct node{
-    int f;
-    string s;
-};
-void solve(){
-    int n,m;
-    cin>>n>>m;
-    vector<node> a(n);
-    for(int i=0;i<n;i++){
-        cin>>a[i].f>>a[i].s;
-    }
-    int tap=0;
-    for(int i=0;i<m;i++){
-        int op,x;
-        cin>>op>>x;
-        if(op==a[tap].f){
-            tap=(tap+n-x)%n;
-        }else{
-            tap=(tap+x)%n;
-        }
-    }
-    cout<<a[tap].s<<'\n';
-}
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    //cin>>t;
-    t=1;
-    while(t--){
-        solve();
-    }
+
+int main() {
+    // 1. 播种：这行代码在整个程序里只要运行一次（通常放在 main 的最开头）
+    srand(time(0)); 
+
+    // 2. 生成随机数
+    // 模拟丢三个骰子 << rand() % 6 + 1 << endl;
+    cout  << rand() % 6 + 1 << endl;
+
+    // 模拟一次攻击伤害 (50 - 100 之间)
+    int damage = rand() % 51 + 50; // % 51 得到 0-50，再加 50 变成 50-100
     
     return 0;
 }
