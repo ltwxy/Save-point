1da24fe39dd937c0012d471348db6b8f15b8f365
ltwxy <3285433514@qq.com>
2026-01-12 19:52:25 +0800

    codeforce/ - 练习文件

---
+++ codeforce/1071_div3/CF_2179_A_Blackslex_and_Password.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int t;
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    cout<<n*k+1<<'\n';
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
